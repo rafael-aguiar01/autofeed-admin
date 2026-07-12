c7e7b135adf9e27d837caef2ba7c097b12364afc
Rafael Aguiar <rafael.a1@hotmail.com>
2026-07-12 13:29:37 -0300

    chore: change api

--- src/store/api/apiSlice.js
+++ src/store/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3336",
+    baseUrl: "https://autofeed.recantoaguiar.com.br/api",
   }),
   endpoints: (builder) => ({}),
 });
